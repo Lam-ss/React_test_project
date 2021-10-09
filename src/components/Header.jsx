9dba2ab5d66d531cb15a3b113bff1ede7c38fe4c
Lam L <ppoo44ttaa@gmail.com>
2021-10-09 23:53:59 +0300

    feat: add header navbar profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://pbs.twimg.com/profile_images/1063925348205821958/DlGcxdOl.jpg'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABbXr4i-QODqhy7tofHYmTYh05rYPktzacw&usqp=CAU' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;

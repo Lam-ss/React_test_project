35577ceaabd5fd70c8d2342a368ac53626f7ec5d
Lam L <ppoo44ttaa@gmail.com>
2021-10-22 21:03:46 +0300

    feat: add pagination

--- src/components/Users/UsersConteiner.jsx
+++ src/components/Users/UsersConteiner.jsx
@@ -1,11 +1,77 @@
 import { connect } from "react-redux";
-import { followAC, unfollowAC, setUsersAC } from "../../redux/users-reducer";
-import Users from "./Users";
+import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from "../../redux/users-reducer";
+// import UsersAPIComponent from "./UsersAPIComponent";
+import * as axios from 'axios';
+import Users from './Users';
+import React from 'react';
+
+
+class UsersContainer extends React.Component {
+
+    // constructor(props) {
+        // super(props);
+
+    //     // axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+    //     //     this.props.setUsers(response.data.items);
+    //     // });
+    //     this.props.setUsers( [
+    //         { id: 1, photoUrl: '../../../my_img/ava.jfif', followed: false, fullName: "Dmitry", status: "I am", location: { city: 'Minsk', country: "Belarus" } },
+    //         { id: 2, photoUrl: '../../../my_img/ava.jfif', followed: true, fullName: "Dmitry2", status: "I am2", location: { city: 'Moscow', country: "Russia" } },
+    //         { id: 3, photoUrl: '../../../my_img/ava.jfif', followed: false, fullName: "Dmitry3", status: "I am3", location: { city: 'Kiev', country: "Ukraine" } }
+    //         ]
+    //     );
+
+    // }
+
+    componentDidMount() {
+        // this.props.setUsers( [
+        //     { id: 1, photoUrl: '../../../my_img/ava.jfif', followed: false, fullName: "Dmitry", status: "I am", location: { city: 'Minsk', country: "Belarus" } },
+        //     { id: 2, photoUrl: '../../../my_img/ava.jfif', followed: true, fullName: "Dmitry2", status: "I am2", location: { city: 'Moscow', country: "Russia" } },
+        //     { id: 3, photoUrl: '../../../my_img/ava.jfif', followed: false, fullName: "Dmitry3", status: "I am3", location: { city: 'Kiev', country: "Ukraine" } }
+        //     ]
+        // );
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+            this.props.setTotalUsersCount(30);
+        });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
+
+
+
+
+
+
+    render() {
+
+        
+
+        return <Users totalUsersCount={this.props.totalUsersCount} 
+                    pageSize={this.props.pageSize}
+                    currentPage={this.props.currentPage}
+                    onPageChanged={this.onPageChanged}
+                    users={this.props.users}
+                    follow={this.props.follow}
+                    unfollow={this.props.unfollow}
+        />
+    }
+}
+
 
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
     }
 }
 
@@ -20,8 +86,14 @@ let mapDispatchToProps = (dispatch) => {
         setUsers: (users) => {
             dispatch(setUsersAC(users));
         },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount))
+        },
     }
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
